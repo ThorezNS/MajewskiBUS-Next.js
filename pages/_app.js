c99bdb16402b1056b6772c2d9abcb1493e0314fa
Maciej_Niemiec <niemiec.maciej@gmail.com>
2022-07-17 16:37:23 +0200

    add metadata with a custom document for the lang attribute

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>MajewskiBUS</title>
+        <meta charSet="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Piotr Majewski & Dariusz Majewski - obsługujemy wycieczki, wesela, transport krajowy i międzynarodowy. Nowy Sącz, Podegrodzie i okolice"
+        />
+        <meta name="author" content="Maciej Niemiec" />
+        <link rel="icon" href="bus-icon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
