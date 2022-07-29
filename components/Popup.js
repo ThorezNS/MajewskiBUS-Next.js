ca48ed5e8cf2ea36906c12ad5e3c7eecd0a9e666
Maciej_Niemiec <niemiec.maciej@gmail.com>
2022-07-29 21:20:46 +0200

    render the text from the popup

--- components/Popup.js
+++ components/Popup.js
@@ -11,6 +11,27 @@ const Popup = ({ isActive, handleToggle }) => {
       <button onClick={handleToggle} className={styles.button}>
         <i className="fa-regular fa-circle-xmark"></i>
       </button>
+      {text.map((section, i) => {
+        return (
+          <div key={i}>
+            <h2 className={styles.title}>{section.title}</h2>
+            {section.body.map((body, j) => {
+              return (
+                <div key={j}>
+                  <p className={styles.paragraph}>{body.paragraph}</p>
+                  {body.bullet.map((bullet, k) => {
+                    return (
+                      <span key={k} className={styles.bullet}>
+                        {bullet}
+                      </span>
+                    );
+                  })}
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
     </div>
   );
 };
