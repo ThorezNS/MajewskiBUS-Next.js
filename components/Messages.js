276a827b22ff330613c10e175871a9e5362e6682
Maciej_Niemiec <niemiec.maciej@gmail.com>
2022-08-01 22:31:08 +0200

    add message text, set the scroll bar to invisible

--- components/Messages.js
+++ components/Messages.js
@@ -1,4 +1,5 @@
 import styles from '../styles/Messages.module.css';
+import messages from '../data/messages.json';
 
 const Messages = () => {
   return (
@@ -7,6 +8,22 @@ const Messages = () => {
         <div className={styles.titleContainer}>
           <i className="fa-solid fa-circle-exclamation"></i>
           <h2 className={styles.title}>Komunikaty</h2>
+        </div>
+        <div className={styles.wrapper}>
+          {messages.map((message, i) => {
+            return (
+              <div className={styles.message} key={i}>
+                <p className={styles.date}>{message.date}</p>
+                {message.body.map((row, j) => {
+                  return (
+                    <p className={styles.text} key={j}>
+                      {row}
+                    </p>
+                  );
+                })}
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
