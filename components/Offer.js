835f8972347d10c46f8d7aeb1eda89b22b7fa6c5
Maciej_Niemiec <niemiec.maciej@gmail.com>
2022-08-10 20:39:23 +0200

    logic responsible for adding a shadow to each offer with time delay

--- components/Offer.js
+++ components/Offer.js
@@ -1,14 +1,31 @@
 import styles from '../styles/Offer.module.css';
 import offer from '../data/offer.json';
+import { useEffect, useState } from 'react';
 
 const Offer = () => {
+  const [textShadowIndex, setTextShadowIndex] = useState(-1);
+
+  useEffect(() => {
+    const isAnIndexNumberLessThenElements = textShadowIndex < offer.text.length;
+    if (isAnIndexNumberLessThenElements) {
+      setTimeout(() => {
+        setTextShadowIndex(textShadowIndex + 1);
+      }, 1500);
+    }
+  }, [textShadowIndex]);
+
   return (
     <div className={styles.offer}>
       <div className={styles.wrapper}>
         <p className={styles.title}>{offer.title}</p>
         {offer.text.map((row, i) => {
           return (
-            <p className={styles.text} key={i}>
+            <p
+              className={`${styles.text} ${
+                i === textShadowIndex ? styles.shadow : ''
+              }`}
+              key={i}
+            >
               {row}
             </p>
           );
