3a891c5e5b9c43f8b6b44fe534f453b5ce60ff49
Maciej_Niemiec <niemiec.maciej@gmail.com>
2022-09-14 21:50:02 +0200

    convert web font imports from CSS to HTML link tag

--- pages/_document.js
+++ pages/_document.js
@@ -4,13 +4,26 @@ export default function Document() {
   return (
     <Html lang="pl-PL">
       <Head />
+      <script
+        src="https://kit.fontawesome.com/3b057449d3.js"
+        crossOrigin="anonymous"
+        defer
+      ></script>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Baloo+Da+2&display=swap"
+        rel="stylesheet"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Alatsi&display=swap"
+        rel="stylesheet"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Genos&display=swap"
+        rel="stylesheet"
+      />
       <body>
         <Main />
         <NextScript />
-        <script
-          src="https://kit.fontawesome.com/3b057449d3.js"
-          crossOrigin="anonymous"
-        ></script>
       </body>
     </Html>
   );
